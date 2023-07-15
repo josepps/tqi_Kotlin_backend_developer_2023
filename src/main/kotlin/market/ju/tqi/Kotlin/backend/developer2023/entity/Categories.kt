5fe712b9574fc6edbeb1f834c16b7ce4fdd0a87d
josepps <joseppsantanafs@gmail.com>
2023-07-15 16:40:55 -0300

    JPA annotations made on tables

--- src/main/kotlin/market/ju/tqi/Kotlin/backend/developer2023/entity/Categories.kt
+++ src/main/kotlin/market/ju/tqi/Kotlin/backend/developer2023/entity/Categories.kt
@@ -1,6 +1,9 @@
 package market.ju.tqi.Kotlin.backend.developer2023.entity
 
+import jakarta.persistence.*
+
+@Entity
 data class Categories(
-    val id: Long? = null,
-    var name: String = ""
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null,
+    @Column(nullable = false, unique = true) var name: String = ""
 )
--- src/main/kotlin/market/ju/tqi/Kotlin/backend/developer2023/entity/products.kt
+++ src/main/kotlin/market/ju/tqi/Kotlin/backend/developer2023/entity/products.kt
@@ -1,13 +1,16 @@
 package market.ju.tqi.Kotlin.backend.developer2023.entity
 
-data class products(
-    val id: Long? = null,
-    var brand: String = "",
-    var name: String = "",
-    var unit_of_measurement: String = "",
-    var measure: Number = 0,
-    var price: Number = 0,
-)
+import jakarta.persistence.*
 
-//Coferir tipo number
-//Como pega o id do outro
+@Entity
+data class Products(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null,
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "category", referencedColumnName = "id")
+    var category: Categories? = null,
+    @Column(nullable = false) var brand: String = "",
+    @Column(nullable = false) var name: String = "",
+    @Column(nullable = false) var unitOfMeasurement: String = "",
+    @Column(nullable = false) var measure: Double = 0.0,
+    @Column(nullable = false) var price: Double = 0.0
+)
