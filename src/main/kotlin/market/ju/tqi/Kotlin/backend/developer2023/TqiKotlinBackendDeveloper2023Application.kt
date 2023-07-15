10cf0cb0c1a4695b1cae68affb7d7921de77d4b9
josepps <joseppsantanafs@gmail.com>
2023-07-15 15:02:45 -0300

    spring project started

---
+++ src/main/kotlin/market/ju/tqi/Kotlin/backend/developer2023/TqiKotlinBackendDeveloper2023Application.kt
@@ -0,0 +1,11 @@
+package market.ju.tqi.Kotlin.backend.developer2023
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TqiKotlinBackendDeveloper2023Application
+
+fun main(args: Array<String>) {
+	runApplication<TqiKotlinBackendDeveloper2023Application>(*args)
+}
---
+++ src/test/kotlin/market/ju/tqi/Kotlin/backend/developer2023/TqiKotlinBackendDeveloper2023ApplicationTests.kt
@@ -0,0 +1,13 @@
+package market.ju.tqi.Kotlin.backend.developer2023
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TqiKotlinBackendDeveloper2023ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
